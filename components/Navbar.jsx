5fecf20af2df2d7a33d62a6e807db934639d335e
JOMADELEMA <jomadelema@gmail.com>
2023-03-30 16:24:53 -0600

    implemented xl ui

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <>
             <div className="hidden lg:flex justify-between gap-10 text-amber-100 mt-3">
-                <div className='flex gap-10 pl-10' >
+                <div className='flex gap-10 pl-10 xl:pl-20 xl:text-xl' >
                     <div className='self-center cursor-pointer hover:underline'>HISTORY</div>
                     <div className='self-center cursor-pointer hover:underline'>DESTINATIONS</div>
                     <div className='self-center cursor-pointer hover:underline'>EXPERIENCES</div>
@@ -16,8 +16,7 @@ const Navbar = () => {
                     </div>
                 </div>
 
-
-                <div className="mr-10 border text-black border-amber-200 bg-amber-300 hover:bg-amber-200 rounded-full flex text-gray font-semibold w-32 h-8 self-center items-center justify-center shadow-md cursor-pointer">
+                <div className="mr-10 xl:mr-20 border text-black border-amber-200 bg-amber-300 hover:bg-amber-200 rounded-full flex text-gray font-semibold w-32 h-8 self-center items-center justify-center shadow-md cursor-pointer">
                     EXPLORE
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,23 +17,33 @@ module.exports = {
         '128': '32rem',
         '160': '36rem',
         '192': '45rem',
+        '224':  '50rem'
 
       },
       backgroundImage: {
         //path must be relative to global css not config.js
-        'arco': "url('../../public/images/bg-arco.jpeg')",
+        'arco': "url('../../public/images/bg-arco.png')",
+        'arco-xl': "url('../../public/images/bg-arco-xl.png')",
         'antigua-square': "url('../../public/images/antigua_square.jpeg')",
         'lago-square': "url('../../public/images/lago_square.jpg')",
         'tikal-square': "url('../../public/images/tikal_square.jpeg')",
         'semuc-square': "url('../../public/images/semuc_square.jpg')",
       }, 
       backgroundPosition: {
+        '-top-1': 'center top -6rem',
+        '-top-2': 'center top -8rem',
+        '-top-3': 'center top -10rem',
         '-top-4': 'center top -12rem',
+      },
+      fontSize: {
+        '10xl': '10rem',
+        '11xl': '12rem',
+        '12xl': '14rem',
       },
       backgroundSize: {
         '150': '175%',
         '16': '4rem',
-      }
+      },
     },
   },
   plugins: [],
